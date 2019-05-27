74d207c14d7eae6e0487060a216091b5f4f11a9b
Matheus <matheus.viana28l@gmail.com>
2019-05-27 00:21:31 -0300

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,10 +4,14 @@
 
 export const environment = {
   production: false,
-  apiUrl: 'http://localhost:8080',
+  apiUrl: 'https://kec-api.herokuapp.com',
+  
+  tokenWhitelistedDomains: [ new RegExp('https://kec-inforstore.herokuapp.com') ],
+  tokenBlacklistedRoutes: []
+  //apiUrl: 'http://localhost:8080',
 
-  tokenWhitelistedDomains: [ new RegExp('localhost:8080') ],
-  tokenBlacklistedRoutes: [ new RegExp('\/oauth\/token') ]
+  //tokenWhitelistedDomains: [ new RegExp('localhost:8080') ],
+  //tokenBlacklistedRoutes: [ new RegExp('\/oauth\/token') ]
 };
 
 /*
