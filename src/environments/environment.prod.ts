fad03af7d280ec42698f8cc13a47071d194833d1
Matheus <matheus.viana28l@gmail.com>
2019-05-27 00:07:46 -0300

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,6 +2,6 @@ export const environment = {
   production: true,
   apiUrl: 'https://kec-api.herokuapp.com',
   
-  tokenWhitelistedDomains: [ new RegExp('localhost:8080') ],
+  tokenWhitelistedDomains: [ new RegExp('https://kec-inforstore.herokuapp.com') ],
   tokenBlacklistedRoutes: [ new RegExp('\/oauth\/token') ]
 };
