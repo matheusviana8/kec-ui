bf3a2d737ffe6c95a86b1f55e3ef804020d1fc5b
Matheus <matheus.viana28l@gmail.com>
2019-05-28 21:28:42 -0300

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,6 +2,6 @@ export const environment = {
   production: true,
   apiUrl: 'https://kec-api.herokuapp.com',
   
-  tokenWhitelistedDomains: [ new RegExp('algamoney-api.herokuapp.com') ],
+  tokenWhitelistedDomains: [ new RegExp('kec-api.herokuapp.com') ],
   tokenBlacklistedRoutes: [ new RegExp('\/oauth\/token') ]
 };
